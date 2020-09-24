a8e1557cc68b44e3606a6c0d245a691712c6fb61
daveringu <david.ringuet@polymtl.ca>
2020-09-24 21:08:07 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,8 @@ def get_word_length_histogram(text):
     histogram=[0]
     for word in text.split():
         lenght = get_num_letters(word)
-        histogram[lenght]
+        histogram[lenght]+=int(lenght !=0)
+        
 	return histogram
 
 def format_histogram(histogram):
