2c0608edf2d5bf038257eb6a6a9c45fa1fd4a2a8
daveringu <david.ringuet@polymtl.ca>
2020-09-24 20:55:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -26,7 +26,7 @@ def format_horizontal_histogram(histogram):
 
 
 if __name__ == "__main__":
-    print(get_num)
+    print(get_num_letter("Hello"))
 	spam = "Stop right there criminal scum! shouted the guard confidently."
 	eggs = get_word_length_histogram(spam)
 	print(eggs, "\n")
